549a6c60f4406e6f70d1e2d74f4abf14cc907750
SUDOARCH <32713884+SUDOARCH@users.noreply.github.com>
2019-06-21 18:14:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-jQuery(document),ready(function(){
+jQuery(document).ready(function(){
   
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'config.json', function() {
